d46857a002ffdce562afecf5697e9ed797eb7330
yomiyou belay <yomiyoubelay18@gamil.com>
2023-10-27 12:17:11 +0300

    sanke logi and glow login completed

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import Sankelogin from './pages/sankelogin';
+import Glowlogin from './pages/glowlogin';
+
+import './App.css';
+export default function App() {
+  return (
+    <>
+   
+    <Sankelogin />
+    <Glowlogin />
+
+    </>
+  )
+    
+
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    boxShadow: {
+      '3xl': '0 0 10px 3px black',
+    }
+  },
+  plugins: [],
+}
